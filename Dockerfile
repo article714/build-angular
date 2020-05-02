b1d50b5b746b59a722b045ab35bf1121f6a5025c
xtof <christophe@certificare.fr>
2020-05-02 10:40:21 +0200

    [ADD] first commit for build-angular

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM node:10-buster-slim
+LABEL maintainer="Certificare C. Guychard<christophe@certificare.fr>"
+
+# Generate locale C.UTF-8 for postgres and general locale data
+ENV LANG C.UTF-8
+
+# Container tooling
+
+COPY container /container
+
+# container building
+
+RUN /container/build.sh 
+
+USER builder
---
+++ container/build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+apt-get update
+apt-get upgrade -yq
+apt-get install -y locales
+localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.UTF-8
+localedef -i fr_FR -c -f UTF-8 -A /usr/share/locale/locale.alias fr_FR.UTF-8
+
+export LANG=en_US.utf8
+
+# Update Packages
+apt-get update \
+    apt-get upgrade -yq
+
+# builder user
+adduser --home /home/builder --disabled-password --shell /bin/bash --gecos "" --uid 3000 --gid 100 builder
+
+# npm install (to save time after)
+
+cd /container/data
+npm install -g
+
+#--
+# Cleaning
+apt-get -yq clean
+apt-get -yq autoremove
+rm -rf /var/lib/apt/lists/*
